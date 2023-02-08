daaf64e78c61096868281e93914fa50c19c9dc24
Skovodka <67179020+Skovodka@users.noreply.github.com>
2023-02-08 10:46:16 +0400

    первая

---
+++ lab1/lab1/Year.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Year
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите год YYYY");
+        string yearSt = Console.ReadLine();
+        int year = Convert.ToInt32(yearSt);
+ 
+        if ((year % 4 == 0) && (year % 100 != 0 || year % 400 == 0))
+        {
+            Console.WriteLine("Високостный год");
+        }
+        else
+        {
+            Console.WriteLine("Невисокостный год");
+        }
+    }
+}
