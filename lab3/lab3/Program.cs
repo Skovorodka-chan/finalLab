aa8bfa61d0c390accf1480bad542f2a5c2e116cc
Skovodka <67179020+Skovodka@users.noreply.github.com>
2023-02-08 10:48:26 +0400

    третья

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,26 @@
+﻿while (true)
+{
+    var arrowsKey = Console.ReadKey();
+    switch (arrowsKey.Key)
+    {
+        case ConsoleKey.UpArrow:
+            Console.Clear();
+            Console.Write("↑");
+            break;
+        
+        case ConsoleKey.DownArrow:
+            Console.Clear();
+            Console.WriteLine("↓");
+            break;
+
+        case ConsoleKey.LeftArrow:
+            Console.Clear();
+            Console.WriteLine("←");
+            break;
+
+        case ConsoleKey.RightArrow:
+            Console.Clear();
+            Console.WriteLine("→");
+            break;
+    }
+}
