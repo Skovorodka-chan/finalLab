22c802b900f7ac5fb979dcc35b7f2f8c962d35b3
Skovodka <67179020+Skovodka@users.noreply.github.com>
2023-02-08 10:47:08 +0400

    вторая

---
+++ lab2/lab2/MathN.cs
@@ -0,0 +1,23 @@
+﻿namespace lab2;
+
+public static class MathN
+{
+    public static bool PrimeN(int number)
+    {
+        if (number == 0 || number == 1) 
+            return false;
+
+        if (number == 2)
+            return true;
+
+        for (int i = 2; i < number; i++)
+        {
+            if ( number % i == 0)
+            {
+                return false;
+            }            
+        }
+        return true;
+    }
+}
+
---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,15 @@
+﻿using lab2;
+
+Console.WriteLine("Введите число от 0 по 10000 включительно");
+
+while (true)
+{
+    string num = Console.ReadLine();
+    int number = Convert.ToInt32(num);
+    if ((number >= 0) && (number <= 10000))
+    {
+        Console.WriteLine($"Число {num} {(MathN.PrimeN(number) ? "простое!" : "не является простым")}");
+        break;
+    }
+    Console.WriteLine("Не то число! Выбири ещё раз!");
+}
